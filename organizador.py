af8e8f3bfec2d74c71a42d11bc3b8dbf41de0f1f
Aiyato <brenan413@gmail.com>
2026-05-08 00:09:55 -0300

    melhoria no organizador de arquivos.

--- organizador.py
+++ organizador.py
@@ -2,43 +2,22 @@ import os
 import shutil   
 pasta = "C:/Users/brenn/Downloads"
 arquivos = os.listdir(pasta)
+
+tipos ={
+    ".pdf": "PDFs",
+    ".jpeg": "JPEGs",
+    ".png": "Imagens",
+    ".ppk": "PPKs",
+}
+
 for arquivo in arquivos:
-    if arquivo.endswith(".pdf"):
-        destino = pasta + "/PDFs"
-        os.makedirs(destino, exist_ok=True)
-
-        origem = pasta + "/" + arquivo
-        novo_destino = destino + "/" + arquivo
-        shutil.move(origem, novo_destino)
-
-        print(f"Arquivo {arquivo} movido!")
     
-    elif arquivo.endswith(".ppk"):
-        destino = pasta + "/PPKs"
-        os.makedirs(destino, exist_ok=True)
-
-        origem = pasta + "/" + arquivo
-        novo_destino = destino + "/" + arquivo
-        shutil.move(origem, novo_destino)
-
-        print(f"Arquivo {arquivo} movido!")
-
-    elif arquivo.endswith(".jpeg"):
-        destino = pasta + "/JPEGs"
-        os.makedirs(destino, exist_ok=True)
-
-        origem = pasta + "/" + arquivo
-        novo_destino = destino + "/" + arquivo
-        shutil.move(origem, novo_destino)
-
-        print(f"Arquivo {arquivo} movido!")
-
-    elif arquivo.endswith(".png"):
-        destino = pasta + "/Imagens"
-        os.makedirs(destino, exist_ok=True)
-
-        origem = pasta + "/" + arquivo
-        novo_destino = destino + "/" + arquivo
-        shutil.move(origem, novo_destino)
-
-        print(f"Arquivo {arquivo} movido!")
+    for extensao, nome_pasta in tipos.items():
+        if arquivo.endswith(extensao):
+            destino = pasta + "/" + nome_pasta
+            os.makedirs(destino, exist_ok=True)
+            origem = pasta + "/" + arquivo
+            novo_destino = destino + "/" + arquivo
+            shutil.move(origem, novo_destino)
+            print(f"Arquivo {arquivo} movido!")
+           
